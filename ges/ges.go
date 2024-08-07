479b95ec129d8bff9fb387004c2a3f75fc73e270
Evandro Jr <evandrojr@gmail.com>
2024-08-07 15:26:57 -0300

    1st commit

---
+++ ges/ges.go
@@ -0,0 +1,60 @@
+package ges
+
+import (
+	"context"
+	"fmt"
+
+	execute "github.com/alexellis/go-execute/v2"
+)
+
+type Outcome struct {
+	ExitCode int
+	Stdout   string
+	Stderr   string
+}
+
+type Shell struct {
+	Name    string
+	Path    string
+	Command string
+}
+
+var shell Shell
+
+func initializeShell() {
+	if shell.Name == "" {
+		shell = Shell{
+			Name:    "zsh",
+			Path:    "/usr/bin/zsh",
+			Command: "zsh",
+		}
+	}
+}
+
+func PrintOutcome(outcome Outcome) {
+	fmt.Printf("stdout:\n%s\nstderr:\n%s\nexit-code: %d\n", outcome.Stdout, outcome.Stderr, outcome.ExitCode)
+}
+
+func ShellExec(command string, showOutcome bool) Outcome {
+	initializeShell()
+	cmd := execute.ExecTask{
+		Command:     shell.Command,
+		Args:        []string{"-c", command},
+		StreamStdio: false,
+	}
+
+	res, err := cmd.Execute(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	outcome := Outcome{
+		res.ExitCode,
+		res.Stdout,
+		res.Stderr,
+	}
+	if showOutcome {
+		fmt.Println("Command:", command)
+		PrintOutcome(outcome)
+	}
+	return outcome
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/evandrojr/go-efficient-shell
+
+go 1.22.5
+
+require (
+	github.com/alexellis/go-execute/v2 v2.2.1
+	github.com/kr/pretty v0.3.1
+)
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+)
